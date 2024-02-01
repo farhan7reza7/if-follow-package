95169596f9cf7a0c26be39583717dd89b3901304
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-01 23:07:35 +0530

    Update follow-back.js

--- src/follow-back.js
+++ src/follow-back.js
@@ -120,17 +120,21 @@ function followBack(yourUsername, yourToken) {
    * @param {Error} error - The error object.
    */
   function handleAxiosError(error) {
-    if (error.response) {
-      // The request was made, but the server responded with an error
-      console.error(
-        `API Error: ${error.response.status} - ${error.response.data.message}`,
-      );
-    } else if (error.request) {
-      // The request was made, but no response was received
-      console.error('No response received from the server');
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        // The request was made, but the server responded with an error
+        console.error(
+          `API Error: ${error.response.status} - ${error.response.data.message}`,
+        );
+      } else if (error.request) {
+        // The request was made, but no response was received
+        console.error('No response received from the server');
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error(`Error: ${error.message}`);
+      }
     } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error(`Error: ${error.message}`);
+      console.error(`Non-Axios Error: ${error.message}`);
     }
   }
 
