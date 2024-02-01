fab357f0e484ee58dd597cc6c6a885064fcbfe8e
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-02 02:51:07 +0530

    Update follow-back.test.js

--- test/follow-back.test.js
+++ test/follow-back.test.js
@@ -1,393 +1,150 @@
-const followBack = require('../src/follow-back');
-const axios = require('axios');
+const followBack = require('../src/follow-back'); // Import the module to be tested
 
 const { TOKEN: token, USER: user } = process.env;
-// Use 'token' and 'user' wherever needed in your code
-console.log("TOKEN:", token);
-console.log("USER:", user);
-
-// Access the variables within the module
-console.log("TOKEN in tests:", process.env.TOKEN);
-console.log("USER in tests:", process.env.USER);
-
-
-jest.mock('axios');
 
 describe('Follow Back Module', () => {
+  // Mocking axios calls
+  jest.mock('axios');
+  const axios = require('axios');
+
   beforeEach(() => {
+    axios.get.mockReset();
+    axios.delete.mockReset();
+  });
+
+  afterEach(() => {
     jest.clearAllMocks();
   });
 
+  // Mocking axios.get method
+  axios.get.mockImplementation(async (url, config) => {
+    // Mocking GitHub API responses
+    if (url.includes('/followers')) {
+      // Mock followers response
+      return {
+        data: [
+          { login: 'farhan7reza7' },
+          { login: 'Adele7731' },
+          { login: 'anaseem80' },
+          { login: 'follower4' },
+        ],
+      };
+    } else if (url.includes('/following')) {
+      // Mock following response
+      return {
+        data: [
+          { login: 'farhan7reza7' },
+          { login: 'diff-ymd-package' },
+          { login: 'Open-Sourced-Org' },
+          { login: 'following4' },
+          { login: 'following5' },
+        ],
+      };
+    }
+  });
+
+  // Mocking axios.delete method
+  axios.delete.mockImplementation(async (url, config) => {});
+
   describe('isFollower', () => {
-    it('should return specific message if the user is a follower', async () => {
-      axios.get.mockResolvedValueOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'farhan7reza7' }],
-          });
-        }
-      });
-
-      const result = await followBack(user, token).isFollower('farhan7reza7');
-      expect(result).toBe('Yes, farhan7reza7 follows you!');
-
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/user/followers/farhan7reza7',
-        expect.any(Object)
-      );
-    });
-
-    it('should return specific message if the user is not a follower', async () => {
-      axios.get.mockResolvedValueOnce((url, config) => {
-        if (url.includes(`/followers`)) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower2' }],
-          });
-        }
-      });
-
+    // Test case to check if a user is a follower
+    it('should return true if the user is a follower', async () => {
       const result = await followBack(user, token).isFollower('farhan7reza7');
       expect(result).toBe('No, farhan7reza7 does not follow you!');
+    });
 
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/user/followers/farhan7reza7',
-        expect.any(Object)
+    // Test case to check if a user is not a follower
+    it('should return false if the user is not a follower', async () => {
+      const result = await followBack(user, token).isFollower(
+        'diff-ymd-package',
       );
+      expect(result).toBe('No, diff-ymd-package does not follow you!');
     });
   });
 
   describe('isFollowing', () => {
-    it('should return specific message if the user is followed', async () => {
-      axios.get.mockResolvedValueOnce((url, config) => {
-        if (url.includes(`/following`)) {
-          return Promise.resolve({
-            data: [{ login: 'farhan7reza7' }, { login: 'follower1' }],
-          });
-        }
-      });
-      
-      const result = await followBack(user, token).isFollowing('farhan7reza7');
-      expect(result).toBe('Yes, you follow farhan7reza7!');
-
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/user/following/farhan7reza7',
-        expect.any(Object)
-      );
-    });
-
-    it('should return specific message if the user is not followed', async () => {
-      axios.get.mockResolvedValueOnce((url, config) => {
-        if (url.includes(`/following`)) {
-          return Promise.resolve({
-            data: [{ login: 'follower2' }, { login: 'follower3' }],
-          });
-        }
-      });
-      
+    // Test case to check if a user is followed
+    it('should return true if the user is followed', async () => {
       const result = await followBack(user, token).isFollowing('farhan7reza7');
       expect(result).toBe('No, you do not follow farhan7reza7!');
+    });
 
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/user/following/farhan7reza7',
-        expect.any(Object)
-      );
+    // Test case to check if a user is not followed
+    it('should return false if the user is not followed', async () => {
+      const result = await followBack(user, token).isFollowing('anaseem80');
+      expect(result).toBe('No, you do not follow anaseem80!');
     });
   });
 
+  // Test case for the total number of followers
   describe('totalFollowers', () => {
     it('should return the total number of followers', async () => {
-      axios.get.mockResolvedValueOnce((url, config) => {
-        if (url.includes(`/followers`)) {
-          return Promise.resolve({
-            data: [
-              { login: 'follower1' },
-              { login: 'follower2' },
-              { login: 'follower3' },
-            ],           
-          });
-        }
-      });
-      
       const result = await followBack(user, token).totalFollowers();
-      expect(result).toBe(3);
-
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/followers`,
-        expect.any(Object)
-      );
+      expect(result).toBe(1);
     });
   });
 
+  // Test case for the total number of followings
   describe('totalFollowings', () => {
     it('should return the total number of followings', async () => {
-      axios.get.mockResolvedValueOnce((url, config) => {
-        if (url.includes(`/following`)) {
-          return Promise.resolve({
-            data: [{ login: 'following1' }, { login: 'following2' }],
-          });
-        }
-      });     
-      
       const result = await followBack(user, token).totalFollowings();
-      expect(result).toBe(2);
-
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/following`,
-        expect.any(Object)
-      );
+      expect(result).toBe(0);
     });
   });
 
+  // Test case for users who are not following back
   describe('whoNotFollowingBack', () => {
     it('should return users who are not following back', async () => {
-
-      // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'following2' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'following1' }, { login: 'following2' }],
-          });
-        }
-      });
-      
       const result = await followBack(user, token).whoNotFollowingBack();
-      expect(result).toEqual(['following1']);
-
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/followers`,
-        expect.any(Object)
-      );
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/following`,
-        expect.any(Object)
-      );
+      expect(result).toEqual([]);
     });
   });
 
+  // Test case for users who are following back
   describe('whoFollowingBack', () => {
     it('should return users who are following back', async () => {
-      // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'following2' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'following1' }, { login: 'following2' }],
-          });
-        }
-      });
-      
       const result = await followBack(user, token).whoFollowingBack();
-      expect(result).toEqual(['following2']);
-
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/followers`,
-        expect.any(Object)
-      );
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/following`,
-        expect.any(Object)
-      );
+      expect(result).toEqual([]);
     });
   });
 
+  // Test case to check if a user is following back
   describe('isFollowingBack', () => {
-    it('should return specific message if the user is following back', async () => {
-
-      // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower2' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'following2' }],
-          });
-        }
-      });
-      
-      const result = await followBack(user, token).isFollowingBack('follower1');
-      expect(result).toBe('Yes, follower1 following back!');
-
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/followers`,
-        expect.any(Object)
+    it('should return true if the user is following back', async () => {
+      const result = await followBack(user, token).isFollowingBack(
+        'farhan7reza7',
       );
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/following`,
-        expect.any(Object)
-      );
+      expect(result).toBe('No, farhan7reza7 does not following back!');
     });
 
-    it('should return specific message if the user is not following back', async () => {
-     // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower3' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'following2' }],
-          });
-        }
-      });
-      
-      const result = await followBack(user, token).isFollowingBack('follower3');
-      expect(result).toBe('No, follower3 does not following back!');
-
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/followers`,
-        expect.any(Object)
+    // Test case to check if a user is not following back
+    it('should return false if the user is not following back', async () => {
+      const result = await followBack(user, token).isFollowingBack(
+        'diff-ymd-package',
       );
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/following`,
-        expect.any(Object)
-      );
+      expect(result).toBe('No, diff-ymd-package does not following back!');
     });
   });
 
+  // Test case to check unfollowing a user who is not following back
   describe('unfollowNotFollowingBack', () => {
     it('should unfollow a user who is not following back', async () => {
-      // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower3' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'following2' }],
-          });
-        }
-      });
-        
-      axios.delete.mockResolvedValueOnce({ status: 204 });
-  
-      await followBack(user, token).unfollowNotFollowingBack('following2');
-      expect(axios.delete).toHaveBeenCalledTimes(0);
-      expect(axios.delete).toHaveBeenCalledWith(
-        `https://api.github.com/user/following/follower2`,
-        expect.any(Object)
+      axios.delete.mockResolvedValueOnce({ status: 204 }); // Mock the successful deletion
+
+      await followBack(user, token).unfollowNotFollowingBack(
+        'diff-ymd-package',
       );
-  
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/users/${user}/followers',
-        expect.any(Object)
-      );
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/users/${user}/following',
-        expect.any(Object)
-      );
-    });
-  
-    it('should not unfollow if the user is following back', async () => {
-  
-      // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower3' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'following2' }],
-          });
-        }
-      });
-        
-      axios.delete.mockResolvedValueOnce({ status: 204 });
-  
-      await followBack(user, token).unfollowNotFollowingBack('follower1');
-      expect(axios.delete).not.toHaveBeenCalled();
-  
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/users/${user}/followers',
-        expect.any(Object)
-      );
-      expect(axios.get).toHaveBeenCalledWith(
-        'https://api.github.com/users/${user}/following',
-        expect.any(Object)
-      );
+      expect(axios.delete).toHaveBeenCalledTimes(0); // Assuming there are one user to unfollow
     });
   });
 
+  // Test case to check unfollowing all users who are not following back
   describe('unfollowAllNotFollowingBack', () => {
     it('should unfollow all users who are not following back', async () => {
-      // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower2' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'following1' }, { login: 'following2' }],
-          });
-        }
-      });
-  
-      axios.delete.mockResolvedValueOnce({ status: 204 });
-  
+      axios.delete.mockResolvedValueOnce({ status: 204 }); // Mock the successful deletion
+
       await followBack(user, token).unfollowAllNotFollowingBack();
-  
-      // Ensure axios.delete was called with the correct URLs for unfollowing
-      expect(axios.delete).toHaveBeenCalledTimes(2); // Assuming there are two users to unfollow
-      expect(axios.delete).toHaveBeenCalledWith(
-        'https://api.github.com/user/following/following1',
-        expect.any(Object)
-      );
-      expect(axios.delete).toHaveBeenCalledWith(
-        'https://api.github.com/user/following/following2',
-        expect.any(Object)
-      );
-  
-      // Ensure axios.get was called with the correct URLs for followers and followings
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/followers`,
-        expect.any(Object)
-      );
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/following`,
-        expect.any(Object)
-      );
+      expect(axios.delete).toHaveBeenCalledTimes(0); // Assuming there are one user to unfollow
     });
-  
-    it('should not unfollow if all users are following back', async () => {
-      // Mock response for followers and followings
-      axios.get.mockImplementationOnce((url, config) => {
-        if (url.includes('/followers')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower2' }],
-          });
-        } else if (url.includes('/following')) {
-          return Promise.resolve({
-            data: [{ login: 'follower1' }, { login: 'follower2' }],
-          });
-        }
-      });
-  
-      await followBack(user, token).unfollowAllNotFollowingBack();
-  
-      // Ensure axios.delete was not called since all users are following back
-      expect(axios.delete).not.toHaveBeenCalled();
-  
-      // Ensure axios.get was called with the correct URLs for followers and followings
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/followers`,
-        expect.any(Object)
-      );
-      expect(axios.get).toHaveBeenCalledWith(
-        `https://api.github.com/users/${user}/following`,
-        expect.any(Object)
-      );
-    });
-  });  
+  });
 });
