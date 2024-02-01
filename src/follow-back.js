29d161a243a71e12483d3b69ce6a2712bff989cc
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-01 23:17:52 +0530

    Update follow-back.js

--- src/follow-back.js
+++ src/follow-back.js
@@ -124,17 +124,17 @@ function followBack(yourUsername, yourToken) {
       if (error.response) {
         // The request was made, but the server responded with an error
         console.error(
-          `API Error: ${error.response.status} - ${error.response.data.message}`,
+          `API Error: ${error.response.status} - ${error.response.data.message || 'No error message available'}`,
         );
       } else if (error.request) {
         // The request was made, but no response was received
         console.error('No response received from the server');
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.error(`Error: ${error.message}`);
+        console.error(`Error: ${error.message || 'No error message available'}`);
       }
     } else {
-      console.error(`Non-Axios Error: ${error.message}`);
+      console.error(`Non-Axios Error: ${error.message || 'No error message available'}`);
     }
   }
 
