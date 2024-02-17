0084aaeb86335557c3d034a9f4c630d946ef8bc6
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-17 19:32:15 +0530

    Update and rename private.js to private-followers.js
    resoolves #158

--- src/private-followers.js
+++ src/private-followers.js
@@ -44,46 +44,4 @@ const handleAxiosError = require("./error-handler");
     return followers;
   }
 
-  /**
-   * Retrieves all users that the specified user is following.
-   * @private
-   * @returns {Promise<Array<string>>} An array of usernames of users being followed.
-   */
-  async function getAllFollowing() {
-    const following = [];
-    let page = 1;
-
-    // Fetch following in paginated manner
-    while (true) {
-      try {
-        const response = await axios.get(
-          `https://api.github.com/users/${yourUsername}/following`,
-          {
-            params: { page, per_page: 100 },
-            auth: {
-              username: yourUsername,
-              password: yourToken,
-            },
-          },
-        );
-
-        const data = response.data;
-
-        // Break if no more following or data
-        if (!data || data.length === 0) {
-          break;
-        }
-
-        // Extract and store usernames of following
-        following.push(...data.map(({ login }) => login));
-        page++;
-        //await delay(1000); // Introduce a delay between API requests
-      } catch (error) {
-        // Handle Axios errors
-        handleAxiosError(error);
-        return [];
-      }
-    }
-
-    return following;
-  }
+module.exports = getAllFollowers;
