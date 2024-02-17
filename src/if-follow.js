c8a1d3bf8f587dcbc596ac8da0705b94d86c6fcd
Farhan Reza <108195448+farhan7reza7@users.noreply.github.com>
2024-02-18 00:43:16 +0530

    Update if-follow.js for following best practices
    Update if-follow.js for following best practices

--- src/if-follow.js
+++ src/if-follow.js
@@ -1,9 +1,35 @@
 // Importing required modules
+
 const axios = require('axios');
 
+/**
+  * Function to Retrieves all followers of the specified user.
+  * @private
+  * @returns {Promise<Array<string>>} An array of usernames of followers.
+  */
 const getAllFollowers = require("./private-followers");
+
+/**
+  * Function to Retrieves all users that the specified user is following.
+  * @private
+  * @returns {Promise<Array<string>>} An array of usernames of users being followed.
+  */
 const getAllFollowing = require("./private-followings");
 
+/**
+ * Class containing utilities for managing followers and followings on GitHub.
+ *
+ * @typedef {Object} IfFollow
+ * @property {Function} isFollower - Checks if a user is following the authenticated user.
+ * @property {Function} isFollowing - Checks if the authenticated user is following a given user.
+ * @property {Function} totalFollowers - Retrieves the total number of followers for the authenticated user.
+ * @property {Function} totalFollowings - Retrieves the total number of users that the authenticated user is following.
+ * @property {Function} whoNotFollowingBack - Retrieves users that the authenticated user is not following back.
+ * @property {Function} whoFollowingBack - Retrieves users that the authenticated user is following back.
+ * @property {Function} isFollowingBack - Checks if a user is following the authenticated user back.
+ * @property {Function} unfollowNotFollowingBack - Unfollows a user who is not following back.
+ * @property {Function} unfollowAllNotFollowingBack - Unfollows all users who are not following back.
+ */
 class IfFollow {
        /**
         * Creates an instance of IfFollow.
